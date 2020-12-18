ad2a74b09411c0e08bc65364bde1e0bcbecddfef
Benjamin Tovar <benjamin.tovar510@gmail.com>
2020-12-18 12:06:22 -0700

    Initial styling of the page. Added FontAwesome and amd getting more comfortable with how things are working together with React.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+//Import Styles
+import "./styles/app.scss";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
