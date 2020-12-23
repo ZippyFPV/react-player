7ae077ec56eb2cb71b5f11a7212ef18963919ec5
Benjamin Tovar <benjamin.tovar510@gmail.com>
2020-12-22 22:13:43 -0700

    Got the array of songs to show up in the console and managed to get the interface set up. Gonna make it pretty once we can play some music

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 //Import Styles
 import "./styles/app.scss";
+//Import Util
+import data from "./data";
 
 function App() {
+  //State
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong} />
     </div>
   );
 }
