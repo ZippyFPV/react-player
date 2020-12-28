96cbb9b041ba1da3568db4286bb3df9cb9c83af4
Benjamin Tovar <benjamin.tovar510@gmail.com>
2020-12-28 12:41:14 -0700

    I can now play and pause the song using state. It is starting to make a bit more sense now. I will be updating the songs current time in the next commit.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,15 @@ function App() {
 
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -6,12 +6,18 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong }) => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   //Ref
   const audioRef = useRef(null);
   //Event Handlers
   const playSongHandler = () => {
-    console.log(audioRef.current);
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
   };
   return (
     <div className="player">
