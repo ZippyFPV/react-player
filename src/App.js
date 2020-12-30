4d906958866b051de57cd59025563bfb6382c5f5
Benjamin Tovar <benjamin.tovar510@gmail.com>
2020-12-30 11:53:09 -0700

    Am now able to change songs and it is reflected on the site. You can play each song as you select it but there is something funky with how the time is shown and it doesn't start playing unless you hit the pause/play button twice. Going to fix that next I think.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
-      <Library songs={songs} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
     </div>
   );
 }
